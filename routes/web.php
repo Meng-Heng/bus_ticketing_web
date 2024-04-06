064945c898c78ed1f7ae16d110a51c8e16417242
Meng-Heng <havmengheng111@gmail.com>
2024-04-06 12:37:38 +0700

    work environment for multi-language

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Stichoza\GoogleTranslate\GoogleTranslate;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/fr', function() {
+    return GoogleTranslate::trans('Bye', 'es');
 });
