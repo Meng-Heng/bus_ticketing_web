4c36901794d87a0a9e06ff14c63ea2e572e126f7
Meng-Heng <havmengheng111@gmail.com>
2024-05-29 10:36:00 +0700

    change: homepage layout

--- resources/views/web/frontend/component/calendar.blade.php
+++ resources/views/web/frontend/component/calendar.blade.php
@@ -1,42 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-    <link id="bs-css" href="https://netdna.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
-    <link id="bsdp-css" href="https://unpkg.com/bootstrap-datepicker@1.9.0/dist/css/bootstrap-datepicker3.min.css" rel="stylesheet">
-</head>
-<body>
-
-    <div class="container">
-        <div class="row">
-            <div class='col-sm-6'>
-                <div class="form-group">
-                    <div class='input-group date' id='datetimepicker1'>
-                        <input type='text' class="form-control" />
-                        <span class="input-group-addon">
-                            <span class="glyphicon glyphicon-calendar"></span>
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <script type="text/javascript">
-                $(function () {
-                    $('#datetimepicker1').datepicker({
-                        format: "mm/dd/yy",
-                        weekStart: 0,
-                        calendarWeeks: true,
-                        autoclose: true,
-                        todayHighlight: true, 
-                        orientation: "auto"
-                    });
-                });
-            </script>
-        </div>
-    </div>
-
-    <script src="https://unpkg.com/bootstrap-datepicker@1.9.0/dist/js/bootstrap-datepicker.min.js"></script>
-
-</body>
-</html>
+<div class="calendar">
+    <header>
+        <h3></h3>
+        <nav>
+            <button id="prev"></button>
+            <button id="next"></button>
+        </nav>
+    </header>
+    <section>
+        <ul class="days">
+            <li>Sun</li>
+            <li>Mon</li>
+            <li>Tue</li>
+            <li>Wed</li>
+            <li>Thu</li>
+            <li>Fri</li>
+            <li>Sat</li>
+        </ul>
+        <ul class="dates">
+            
+        </ul>
+    </section>
+</div>
--- resources/views/web/frontend/section/home/ticket.blade.php
+++ resources/views/web/frontend/section/home/ticket.blade.php
@@ -1,17 +1,13 @@
-<div class="form-box align-items-center">
+<div class="ticket-box my-5 mx-6">
         <form class="row g-3" id="ticket-box">
             <h2>Where are you heading?</h2>
-            <div class="col-md-5">
+            <div class="col-md-10">
                 <div class="input-group origin-box">
-                    <input id="txtBusOrigin" name="txtOrigin" class="form-control search-control" type="text" placeholder="Leaving from..." autocomplete="off">
-                        <span class="input-group-btn">
-                            <a class="btn btn-default btn-search-control-addon" id="btnRemoveBusOrigin">
-                                <span class="glyphicon glyphicon-remove">
-                                    <!-- Insert -->
-                                </span>
-                            </a>
-                        </span>
-                    </input>
+                    <div class="row">
+                        <i class="fas fa-location md-3"></i>
+                        <input id="txtBusOrigin" name="txtOrigin" class="form-control origin md-5" type="text" placeholder="Leaving from..." autocomplete="off">
+                        </input>
+                    </div>
                 </div>
             </div>
             <!-- @include('web.frontend.component.calendar') -->
